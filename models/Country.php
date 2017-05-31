03d1ce1471828b0fc796763f1ee2b620076225eb
Darrell Ulm <darrellulm@gmail.com>
2017-05-31 14:02:25 -0400

    Fix issue with country

--- models/Country.php
+++ models/Country.php
@@ -2,12 +2,47 @@
 
 namespace app\models;
 
-use yii\db\ActiveRecord;
+use Yii;
 
-class Country extends ActiveRecord
+/**
+ * This is the model class for table "country".
+ *
+ * @property string $code
+ * @property string $name
+ * @property integer $population
+ */
+class Country extends \yii\db\ActiveRecord
 {
-  public static function tableName()
-  {
-    return '{{%country}}';
-  }
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'country';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['code', 'name'], 'required'],
+            [['population'], 'integer'],
+            [['code'], 'string', 'max' => 2],
+            [['name'], 'string', 'max' => 52],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'code' => 'Code',
+            'name' => 'Name',
+            'population' => 'Population',
+        ];
+    }
 }
