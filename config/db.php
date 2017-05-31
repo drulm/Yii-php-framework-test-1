4932051621837e852ec9225530ad47268e35a699
Darrell Ulm <darrellulm@gmail.com>
2017-05-31 10:28:42 -0400

    A bit of working framework

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=basic',
+    'username' => 'root',
+    'password' => '86punish',
+    'charset' => 'utf8',
+];
---
+++ models/Country.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+
+class Country extends ActiveRecord
+{
+  public static function tableName()
+  {
+    return '{{%country}}';
+  }
+}
