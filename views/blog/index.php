b2ed7ff7887ee10c9ee08e614230b9a65d3c9068
Darrell Ulm <darrellulm@gmail.com>
2017-05-31 15:45:48 -0400

    crud for simple blog app

---
+++ views/blog/index.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\BlogSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Blogs';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="blog-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Blog', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'title',
+            'text:ntext',
+            'id',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
