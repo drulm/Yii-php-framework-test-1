db8b1cb417d5637054268e762948599cc0f02599
Darrell Ulm <darrellulm@gmail.com>
2017-06-02 13:38:09 -0400

    .gitignore update

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=basic',
-    'username' => 'username',
-    'password' => 'password',
-    'charset' => 'utf8',
-];
